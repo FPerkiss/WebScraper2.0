077514630d8f48c47b3c5287606b09f74e88fac9
Frank Perkiss <me@frankperkiss.co.uk>
2022-04-06 16:58:19 +0100

    working - user selects rooms / location / prop type.

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ prop_location = []
 prop_price = []
 prop_type = []
 prop_id = []
+prop_rooms = []
 url_pageNo = 0
 prop_NoOfListings = None
 prop_listings = None
+prop_added = 0
+prop_tempCount = 0
 
 # User selected variables [No of bedrooms, property type, location]
 userSelected_rooms = None
@@ -38,44 +41,65 @@ while locationTrue == 0:
     else:
         print("Please try a different location.")
 
-# This url is used to get the properties with the parameters we want.
-URL = f"https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%{userSelected_location}&maxBedrooms={userSelected_rooms}&minBedrooms={userSelected_rooms}&radius=0.0&index={url_pageNo}&propertyTypes={userSelected_type}&secondaryDisplayPropertyType=detachedshouses&includeSSTC=true&mustHave=&dontShow=&furnishTypes=&keywords="
-page = r.get(URL)
-soup = BeautifulSoup(page.content, "html.parser")
+def urlReload():
+    global url_pageNo
+    global URL
+    global soup
+    # This url is used to get the properties with the parameters we want.
+    URL = f"https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%{userSelected_location}&maxBedrooms={userSelected_rooms}&minBedrooms={userSelected_rooms}&radius=0.0&index={url_pageNo}&propertyTypes={userSelected_type}&secondaryDisplayPropertyType=detachedshouses&includeSSTC=true&mustHave=&dontShow=&furnishTypes=&keywords="
+    page = r.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    url_pageNo = url_pageNo+24
 
-# Gets us the number of listings:
-prop_NoOfListings = soup.find("span", {"class": "searchHeader-resultCount"})
-prop_NoOfListings = prop_NoOfListings.get_text()
-prop_NoOfListings = int(prop_NoOfListings.replace(",", ""))
-print(prop_NoOfListings)
+def findAdd():
+    global prop_added
+    global prop_tempCount
+    global prop_NoOfListings
+    # Gets us the number of listings:
+    prop_NoOfListings = soup.find("span", {"class": "searchHeader-resultCount"})
+    prop_NoOfListings = prop_NoOfListings.get_text()
+    prop_NoOfListings = int(prop_NoOfListings.replace(",", ""))
+    print(prop_NoOfListings)
+    prop_search = soup.find(id="propertySearch")
+    container = prop_search.find(id="propertySearch-results-container")
+    search_results = container.find(id="l-searchResults")
+    prop_listings = search_results.find_all('div', class_="l-searchResult is-list")
 
-prop_search = soup.find(id="propertySearch")
-container = prop_search.find(id="propertySearch-results-container")
-search_results = container.find(id="l-searchResults")
-prop_listings = search_results.find_all('div', class_="l-searchResult is-list")
+    for prop_listing in prop_listings:
+        soupLocation = prop_listing.find('meta', itemprop="streetAddress")
+        soupPrice = prop_listing.find("div", class_="propertyCard-priceValue")
+        soupID = prop_listing.find('a', "propertyCard-anchor", "id")
+        soupID = soupID.attrs['id']
 
-for prop_listing in prop_listings:
-    soupLocation = prop_listing.find('meta', itemprop="streetAddress")
-    soupPrice = prop_listing.find("div", class_="propertyCard-priceValue")
-    soupID = prop_listing.find('a', "propertyCard-anchor", "id")
-    soupID = soupID.attrs['id']
-
-
-
-    if soupID not in prop_id:
         prop_location.append(soupLocation['content'])
         prop_price.append(soupPrice.text)
         prop_id.append(soupID)
+        prop_rooms.append(userSelected_rooms)
+        prop_added = prop_added +1
+        prop_tempCount = prop_tempCount +1
+
+def exportPanadas():
+    # Data export to a .CSV file using the Pandas library.
+    data = {
+        "Location": panda_location,
+        "Price": prop_price,
+        "Type": userSelected_type,
+        "rooms": prop_rooms,
+        "Property ID": prop_id,
+    }
+    df = pd.DataFrame.from_dict(data)
+    df.to_csv(rf"{panda_location} - {userSelected_type}.csv", encoding="utf-8")
 
 
-# Data export to a .CSV file using the Pandas library.
-data = {
-    "Location": panda_location,
-    "Price": prop_price,
-    "Type": userSelected_type,
-    "rooms": userSelected_rooms,
-    "Property ID": prop_id,
-}
-df = pd.DataFrame.from_dict(data)
-df.to_csv(r"TestDataNEW.csv", encoding="utf-8")
+urlReload()
+findAdd()
+print(f"{prop_tempCount} / {prop_NoOfListings} START")
 
+while prop_added < prop_NoOfListings:
+    urlReload()
+    findAdd()
+    print(f"{prop_added} / {prop_NoOfListings} WHILE")
+
+
+
+exportPanadas()
